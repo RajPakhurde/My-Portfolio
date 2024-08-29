5b4f658f6470ba38ea489db380c88826abed3107
Raj Pakhurde <rajofficial4803@gmail.com>
2024-08-29 21:25:51 +0530

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+       <h1 className="bg-amber-500 text-5xl ">Hello world</h1>
+       <h2 className="text-3xl text-red-300">Hello world 2</h2>
+    </div>
+  )
+}
+
+export default App
